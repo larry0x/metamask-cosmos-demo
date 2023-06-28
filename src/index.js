4f1d02085d3a91f9bac4937b1131edef6d3f1813
larry <26318510+larry0x@users.noreply.github.com>
2023-06-28 20:50:41 +0100

    broadcast tx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { Registry } from "@cosmjs/proto-signing";
-import { StargateClient, defaultRegistryTypes } from "@cosmjs/stargate";
+import { StargateClient } from "@cosmjs/stargate";
+import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx";
 import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
 import { SignMode } from "cosmjs-types/cosmos/tx/signing/v1beta1/signing";
-import { AuthInfo, SignDoc, SignerInfo } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { AuthInfo, SignDoc, SignerInfo, Tx, TxBody } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 import { bech32 } from "bech32";
-import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import Long from "long";
 
 const fromAddrInput = document.getElementById("fromAddr");
 const toAddrInput = document.getElementById("toAddr");
@@ -26,6 +26,10 @@ function encodeHex(bytes) {
   return [...bytes].map((byte) => byte.toString(16).padStart(2, "0")).join("");
 }
 
+function encodeJson(obj) {
+  return new TextEncoder().encode(JSON.stringify(obj));
+}
+
 function decodeHex(hex) {
   if (hex.startsWith("0x")) {
     hex = hex.slice(2);
@@ -36,12 +40,6 @@ function decodeHex(hex) {
 submitBtn.addEventListener("click", async function (event) {
   event.preventDefault();
 
-  // interface registry - used to encode protobuf types
-  const registry = new Registry(defaultRegistryTypes);
-
-  // stargate client - used to broadcast transaction
-  // const client = await StargateClient.connect(rpcEndpoint);
-
   const fromAddress = fromAddrInput.value;
   const fromAddrBytes = addressBytesFromBech32(fromAddress);
 
@@ -50,26 +48,25 @@ submitBtn.addEventListener("click", async function (event) {
     value: new Uint8Array([10, 32, ...fromAddrBytes]), // a little hack to encode the pk into proto bytes
   };
 
-  const msg = {
-    typeUrl: "/cosmos.bank.v1beta1.MsgSend",
-    value: {
-      fromAddress,
-      toAddress: toAddrInput.value,
-      amount: [
-        Coin.fromPartial({
-          denom: denomInput.value,
-          amount: amountInput.value,
-        }),
-      ],
-    }
-  };
+  const msg = MsgSend.fromPartial({
+    fromAddress,
+    toAddress: toAddrInput.value,
+    amount: [
+      Coin.fromPartial({
+        denom: denomInput.value,
+        amount: amountInput.value,
+      }),
+    ],
+  });
 
-  const body = {
-    typeUrl: "/cosmos.tx.v1beta1.TxBody",
-    value: {
-      messages: [msg],
-    },
-  };
+  const body = TxBody.fromPartial({
+    messages: [
+      {
+        typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+        value: MsgSend.encode(msg).finish(),
+      },
+    ],
+  });
 
   const authInfo = AuthInfo.fromPartial({
     signerInfos: [
@@ -86,13 +83,11 @@ submitBtn.addEventListener("click", async function (event) {
     fee: {
       amount: [],
       gasLimit: 200000,
-      payer: "",
-      granter: "",
     },
   });
 
   const signDoc = SignDoc.fromPartial({
-    bodyBytes: registry.encode(body),
+    bodyBytes: TxBody.encode(body).finish(),
     authInfoBytes: AuthInfo.encode(authInfo).finish(),
     chainId: chainIdInput.value,
     accountNumber: accountNumberInput.value,
@@ -110,9 +105,29 @@ submitBtn.addEventListener("click", async function (event) {
     params: [signBytesHex, accounts[0]],
   });
 
+  const sigBytes = decodeHex(sigHex);
+  const r = sigBytes.slice(0, 32);
+  const s = sigBytes.slice(32, 64);
+  const v = sigBytes[64];
+
+  const cred = {
+    r: Long.fromBytesBE(v, true).toString(),
+    s: Long.fromBytesBE(s, true).toString(),
+    v,
+  };
+
   const tx = Tx.fromPartial({
     body,
     authInfo,
-    signatures: [decodeHex(sigHex)],
+    signatures: [encodeJson(cred)],
   });
+
+  const client = await StargateClient.connect(rpcEndpoint);
+
+  try {
+    const txhash = await client.broadcastTxSync(Tx.encode(tx).finish());
+    console.log("tx broadcasted! txhash:", txhash);
+  } catch (err) {
+    console.log("tx failed!", err.toString());
+  }
 });
